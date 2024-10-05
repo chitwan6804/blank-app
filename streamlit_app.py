780f39e7098e38a0033a1fd669013ab67d158295
Chitwan <128251060+chitwan6804@users.noreply.github.com>
2024-10-05 15:08:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-.\venv\Scripts\activate         # Activate the virtual environment (Windows)
-pip install scikit-learn       # Install scikit-learn
 streamlit run streamlit_app.py 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
