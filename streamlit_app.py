a8b5839f4c5db9dcd109bb704240f652e3f01b49
Chitwan <128251060+chitwan6804@users.noreply.github.com>
2024-10-05 15:01:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.write('Starting the app...')
 
-import os
-st.write(os.getcwd())
+@st.cache_data
+def load_data():
+    return pd.read_csv('heart.csv')
 
+data = load_data()
